1d0a71825a7d98981cf99402b3cfa885f1455629
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-11-26 21:29:03 +0800

    Create a python-basics.py in intro-to-python dir

---
+++ introduction-to-python/python-basics.py
@@ -0,0 +1,70 @@
+"""
+The Python Interface
+Hit Run Code to run your first Python code with Datacamp and see the output!
+
+Notice the script.py window; this is where you can type Python code to solve exercises. You can hit Run Code and Submit Answer as often as you want. If you're stuck, you can click Get Hint, and ultimately Get Solution.
+
+You can also use the IPython Shell interactively by typing commands and hitting Enter. Here, your code will not be checked for correctness so it is a great way to experiment.
+
+
+Instructions
+Experiment in the IPython Shell; type 5 / 8, for example.
+Add another line of code to script.py, print(7 + 10), to be checked for correctness.
+Hit Submit Answer to execute the Python script and receive feedback.
+"""
+
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(7 + 10)
+
+
+
+""" Exercise
+Any comments?
+You can also add comments to your Python scripts. Comments are important to make sure that you and others can understand what your code is about and do not run as Python code.
+
+They start with # tag. See the comment in the editor, # Division; now it's your turn to add a comment!
+
+Instructions
+Above the print(7 + 10), add the comment
+
+# Addition
+
+"""
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+
+
+"""
+Python as a calculator
+Python is perfectly suited to do basic calculations. It can do addition, subtraction, multiplication and division.
+
+The code in the script gives some examples.
+
+Now it's your turn to practice!
+
+Instructions
+Print the sum of 4 + 5.
+Print the result of subtracting 5 from 5.
+Multiply 3 by 5.
+Divide 10 by 2.
+"""
+
+# Addition
+print(4 + 5)
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Division
+print(10 / 2)
