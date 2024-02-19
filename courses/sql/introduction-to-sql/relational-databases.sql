bd2c46199e7aa607aa8ecd1454bc0562a8944e7d
SiegfredLorelle <siegfredlorelle09@gmail.com>
2024-02-20 05:00:32 +0800

    Finish relational databases

---
+++ courses/sql/introduction-to-sql/relational-databases.sql
@@ -0,0 +1,10 @@
+/* Our very own table
+A database has been set up for this course and the books table is available here.
+
+Run the code to explore what data books holds!
+
+Instructions
+Hit "Run Code" to see the books table. */
+
+SELECT *
+FROM books;
